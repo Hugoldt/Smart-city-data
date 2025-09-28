e23c68be9bc215b7562135de7308d99b3db86b25
Antoine Chopard <antoinechopard@macbookpro-1.home>
2025-09-28 16:55:12 +0200

    Mise à jour de App.tsx

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import MapScreen from './heartsafe/screens/MapScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Map">
+        <Stack.Screen name="Map" component={MapScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
